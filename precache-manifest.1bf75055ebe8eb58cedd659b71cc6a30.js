1ceaa80e7fe224accf2116aa94f468f7139fba75
Kyle <kylennardsy@hotmail.com>
2020-05-02 03:13:41 -0400

    Updates

---
+++ precache-manifest.1bf75055ebe8eb58cedd659b71cc6a30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d770a6cfd2829247dadcdb211640d1f",
+    "url": "/REPOSITORY_NAME/index.html"
+  },
+  {
+    "revision": "77db2a40ca1cd717575f",
+    "url": "/REPOSITORY_NAME/static/css/main.450fd0e5.chunk.css"
+  },
+  {
+    "revision": "a99ccc740f7e57f502f0",
+    "url": "/REPOSITORY_NAME/static/js/2.8d0c7e50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REPOSITORY_NAME/static/js/2.8d0c7e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77db2a40ca1cd717575f",
+    "url": "/REPOSITORY_NAME/static/js/main.63301b8c.chunk.js"
+  },
+  {
+    "revision": "8636f7cf23f8ecf3d132",
+    "url": "/REPOSITORY_NAME/static/js/runtime-main.47cdd3dd.js"
+  }
+]);
